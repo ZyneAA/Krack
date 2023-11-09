b3b448c254babc794ff092181827a12b23500166
ZyneAA <htet.zyne.aa@gmail.com>
2023-11-09 09:36:25 +0630

    fixed the slah command error

--- cogs/music/util.py
+++ cogs/music/util.py
@@ -4,7 +4,7 @@ MAXIMU_QUEUE_SIZE_REACHED = "Sorry your queue is full."
 MINIMUM_QUEUE_SIZE_REACHED = "Queue is empty."
 
 
-def make_duraion(self, length):
+def make_duraion(length):
     seconds = length / 1000
     minutes = seconds // 60
     remaining_seconds = seconds % 60
